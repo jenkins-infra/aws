02a02ce559e2d4c9fd5403b8872a26acd93373db
Damien Duportal <damien.duportal@gmail.com>
2021-01-28 10:04:54 +0100

    Upgrade providers aws and local + apply constraints
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- versions.tf
+++ versions.tf
@@ -1,8 +1,14 @@
 
 terraform {
   required_version = ">= 0.13.5, <0.14"
-}
-
-provider "local" {
-  version = "~> 1.4"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+    local = {
+      source = "hashicorp/local"
+      version = "~> 2.0.0"
+    }
+  }
 }

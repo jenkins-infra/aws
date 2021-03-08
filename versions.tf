da83a6544bba1bab9f10e07ba4380429c0950d52
Damien Duportal <damien.duportal@gmail.com>
2021-03-08 13:27:15 +0100

    Terraform fmt
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- versions.tf
+++ versions.tf
@@ -11,7 +11,7 @@ terraform {
       version = "~> 2.0.0"
     }
     kubernetes = {
-      source = "hashicorp/kubernetes"
+      source  = "hashicorp/kubernetes"
       version = "~> 2.0.2"
     }
   }

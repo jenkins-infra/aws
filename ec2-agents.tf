5f661d587840eb9e6a4ca09458ca2edc68815031
Damien Duportal <damien.duportal@gmail.com>
2021-05-20 17:26:00 +0200

    feat: add custom secgroup for release.ci ec2 agents
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- ec2-agents.tf
+++ ec2-agents.tf
@@ -59,3 +59,48 @@ resource "aws_security_group" "ec2_agents_infraci" {
     jenkins = "infra.ci.jenkins.io"
   }
 }
+
+resource "aws_security_group" "ec2_agents_release" {
+  name        = "ec2_agents_release"
+  description = "Allow release.ci to connect to EC2 agents"
+
+  ingress {
+    description = "Allow SSH from release.ci"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [
+      "52.177.88.13/32" # AKS cluster outside IP
+    ]
+  }
+
+  ## egress for DNS, HTTP, HTTPS and SSH only
+  egress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] #tfsec:ignore:AWS009
+  }
+  egress {
+    from_port   = 53
+    to_port     = 53
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"] #tfsec:ignore:AWS009
+  }
+  egress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] #tfsec:ignore:AWS009
+  }
+  egress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] #tfsec:ignore:AWS009
+  }
+
+  tags = {
+    jenkins = "release.ci.jenkins.io"
+  }
+}

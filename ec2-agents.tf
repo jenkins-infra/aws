c7f71e8e41677030b2fe6b6d5130d1708593f684
Damien Duportal <damien.duportal@gmail.com>
2021-05-20 17:25:31 +0200

    feat: define new public keys per Jenkins controller for conencting to EC2 agents
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- ec2-agents.tf
+++ ec2-agents.tf
@@ -1,11 +1,17 @@
 
-# This keypair shall be used to allow the Jenkins controller infra.ci to connect to the EC2 agents
-resource "aws_key_pair" "ec2_agents_infraci" {
-  key_name   = "ec2_agents_infraci"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDYlEZmGs43NHtJysZpSJX/THB2JBcCP0o+8ijJVAADhzuRYE5Jdd+dwdCWyGzGc8r2pxdw+1l3NHHkhRqTWIgH9h77VLnavrqDf6dTuC05q4RTBw5E6AknKB3L0a4IzpXtvT+MbdkkJaNEFWpK63oGSxbO2kaTUXzYiJg/BnpMo8gKyeF1t1RBZ249xgWqmb4SDkM7hOLC7HF8rJjHXNFvhBAm99aWwr0MhFwmEp6fRAiI5AC52owIewXN9dkXs8oMXjQ4J6g7dO5Vwdu2M19Uk0DbaqEJmngoSYtOikpkFSlnJO3iOqYUXh6uEZLd1OjZbxbcvnmxMlI6UfKkVaKGWzLb6G8yz+Ahz9oxKmq11QsUoMedHZlwHNgPpJ+TS3p5TlDZYGsuVRm7EnMN0+KecXzoZk6i/kJO+uQ0oMxdtZCYVlwdTiOW/85WqB7lSVokIWAkRmV89kHEbgSJLgrH9aXr4xw2jhzN+F9HT2Z5mOoVU3Exkursu/bXhdocIhdtXQVBDIA0hcvouUGar6Db7VK6NQeJoAWUVVELhBZP+rGhbg5WjGZANPPi01VMIDhks5KXZt0AtaQxpZCaynbasm0g7fHvy+BP3bTQcpa95hBWYL0tjon5BPLTkzK4q282pQluba0bralLDgF0SoNw4F83w/hu7YwHMoSC9Njzcw== jenkins-infra@jenkins.io"
+
+## Load public keypars from the reference file
+# Each line is expected to holds an OpenSSH public key followed by a comment character ('#') and the name of the instance using the ec2 agents with this key
+locals {
+  ec2_agents_publickeys = compact(split("\n", file("./ec2_agents_authorized_keys")))
+}
+resource "aws_key_pair" "ec2_agents" {
+  for_each   = toset(local.ec2_agents_publickeys)
+  key_name   = "ec2_agents_${trimspace(element(split("#", each.key), 1))}"
+  public_key = trimspace(element(split("#", each.key), 0))
 
   tags = {
-    jenkins = "infra.ci.jenkins.io"
+    jenkins = trimspace(element(split("#", each.key), 1))
   }
 }
 

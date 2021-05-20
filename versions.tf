7a404fd46cc2fe60b4eb79a0c4fb5b3af260c89b
Damien Duportal <damien.duportal@gmail.com>
2021-05-20 17:24:51 +0200

    chore: bump modules versions to latest
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.37"
+      version = "~> 3.42"
     }
     local = {
       source  = "hashicorp/local"
@@ -12,7 +12,7 @@ terraform {
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.1"
+      version = "~> 2.2"
     }
   }
 }

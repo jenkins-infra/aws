069a337191ee80879d520231a60edc096d44e384
updatebot <updatebot@olblak.com>
2021-11-25 19:15:31 +0000

    chore: Updated the file "/tmp/updatecli/jenkins-infra/aws/eks-cluster.tf"
    Made with ❤️️  by updatecli

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -5,7 +5,7 @@ resource "aws_kms_key" "eks" {
 
 module "eks" {
   source       = "terraform-aws-modules/eks/aws"
-  version      = "~> 17.20"
+  version      = "17.24.0"
   cluster_name = local.cluster_name
   # From https://docs.aws.amazon.com/eks/latest/userguide/kubernetes-versions.html
   cluster_version = var.kubernetes_version

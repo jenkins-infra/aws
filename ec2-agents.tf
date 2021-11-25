32e58584b085f685cba8f424bfa95611ea36929d
Damien Duportal <damien.duportal@gmail.com>
2021-11-25 20:25:05 +0100

    chore(iam) migrate jenkins_agents IAM setup to tf file
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- ec2-agents.tf
+++ ec2-agents.tf
@@ -9,9 +9,44 @@ resource "aws_iam_policy" "jenkins_ec2_agents" {
   path        = "/"
   description = "IAM Policy to allow a Jenkins Controller to start and manage EC2 agents with the 'ec2' plugin."
 
-  ## tfsec rule AWS099 is ignored: the IAM policy cannot now the EC2 resources in advance
-  policy = file("./iam-policies/jenkins-ec2-agents.json") #tfsec:ignore:AWS099
+  policy = data.aws_iam_policy_document.jenkins_ec2_agents.json
 }
+
+data "aws_iam_policy_document" "jenkins_ec2_agents" {
+  statement {
+    sid    = "Stmt1312295543082"
+    effect = "Allow"
+
+    actions = [
+      "ec2:DescribeSpotInstanceRequests",
+      "ec2:DescribeSpotPriceHistory",
+      "ec2:CancelSpotInstanceRequests",
+      "ec2:GetConsoleOutput",
+      "ec2:RequestSpotInstances",
+      "ec2:RunInstances",
+      "ec2:StartInstances",
+      "ec2:StopInstances",
+      "ec2:TerminateInstances",
+      "ec2:CreateTags",
+      "ec2:DeleteTags",
+      "ec2:DescribeInstances",
+      "ec2:DescribeKeyPairs",
+      "ec2:DescribeRegions",
+      "ec2:DescribeImages",
+      "ec2:DescribeAvailabilityZones",
+      "ec2:DescribeSecurityGroups",
+      "ec2:DescribeSubnets",
+      "iam:ListInstanceProfilesForRole",
+      "iam:PassRole",
+      "ec2:GetPasswordData"
+    ]
+
+    # TODO: list all resources and remove the tfsec ignore rule
+    #tfsec:ignore:AWS099
+    resources = ["*"]
+  }
+}
+
 
 resource "aws_iam_user_policy_attachment" "allow_ec2_on_infraci" {
   user       = data.aws_iam_user.jenkins_infra_ci.user_name

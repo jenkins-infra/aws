1998055a6f8ccc5dba0b008bf233ad661e4608a5
smerle33 <smerle@cloudbees.com>
2022-02-10 18:43:37 +0100

    dynamically generate the aws_security_group from local map variable

--- ec2-agents.tf
+++ ec2-agents.tf
@@ -73,6 +73,80 @@ resource "aws_key_pair" "ec2_agents" {
   }
 }
 
+## dynamically construct aws security groups
+resource "aws_security_group" "ec2_agents_${trimspace(element(split(":", each.key),0))}" {
+  for_each   = toset(local.aws_security_groups)
+  
+  name        = "ec2_agents_${trimspace(element(split(":", each.key),0))}"
+  description = "Allow ${trimspace(element(split(":", each.key),1))} to connect to EC2 agents"
+
+  ingress {
+    description = "Allow SSH from ${trimspace(element(split(":", each.key),1))}"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [
+      "${trimspace(element(split(":", each.key),2))}" # this AKS cluster outside IP
+    ]
+  }
+
+  ## egress for DNS, HTTP, HTTPS and SSH only
+  egress {
+    description = "Allow outgoing SSH requests from agents"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] #tfsec:ignore:AWS009
+  }
+  egress {
+    description = "Allow outgoing DNS requests from agents"
+    from_port   = 53
+    to_port     = 53
+    protocol    = "udp"
+    cidr_blocks = ["0.0.0.0/0"] #tfsec:ignore:AWS009
+  }
+  egress {
+    description = "Allow outgoing HTTP requests from agents"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] #tfsec:ignore:AWS009
+  }
+  egress {
+    description = "Allow outgoing HTTPS requests from agents"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] #tfsec:ignore:AWS009
+  }
+  egress {
+    description = "Allow outgoing JNLP requests from agents"
+    from_port   = 50000
+    to_port     = 50000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] #tfsec:ignore:AWS009
+  }
+  egress {
+    description = "Allow outgoing WinRM HTTP requests from agents"
+    from_port   = 5985
+    to_port     = 5985
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] #tfsec:ignore:AWS009
+  }
+  egress {
+    description = "Allow outgoing WinRM HTTPS requests from agents"
+    from_port   = 5986
+    to_port     = 5986
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] #tfsec:ignore:AWS009
+  }
+
+  tags = {
+    jenkins = "${trimspace(element(split(":", each.key),1))}"
+  }
+}
+
+
 resource "aws_security_group" "ec2_agents_infraci" {
   name        = "ec2_agents_infraci"
   description = "Allow infra.ci to connect to EC2 agents"

b716ef4a1980474aa545f6eb386464789ab0f532
Hervé Le Meur <hlemeur@cloudbees.com>
2022-02-14 12:02:30 +0100

    debug: no coredns

--- eks-addons.tf
+++ eks-addons.tf
@@ -5,11 +5,11 @@ resource "aws_eks_addon" "kube-proxy" {
   addon_name   = "kube-proxy"
 }
 
-# https://docs.aws.amazon.com/eks/latest/userguide/managing-coredns.html
-resource "aws_eks_addon" "coredns" {
-  cluster_name = module.eks.cluster_id
-  addon_name   = "coredns"
-}
+# # https://docs.aws.amazon.com/eks/latest/userguide/managing-coredns.html
+# resource "aws_eks_addon" "coredns" {
+#   cluster_name = module.eks.cluster_id
+#   addon_name   = "coredns"
+# }
 
 # https://docs.aws.amazon.com/eks/latest/userguide/pod-networking.html
 resource "aws_eks_addon" "vpc-cni" {

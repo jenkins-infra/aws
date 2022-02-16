c87f32643345ac820abc05073cad60c61f58eee3
updatebot <updatebot@olblak.com>
2022-02-16 23:23:39 +0000

    chore: Updated the file "/tmp/updatecli/jenkins-infra/aws/eks-role-auto...
    ... scaler.tf"
    
    Made with ❤️️ by updatecli

--- eks-role-autoscaler.tf
+++ eks-role-autoscaler.tf
@@ -1,6 +1,6 @@
 module "eks_iam_role_autoscaler" {
   source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version                       = "4.11.0"
+  version                       = "4.12.0"
   create_role                   = true
   role_name                     = "cluster-autoscaler"
   provider_url                  = replace(module.eks.cluster_oidc_issuer_url, "https://", "")

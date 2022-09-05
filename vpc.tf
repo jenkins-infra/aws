db05f80a6c2a16c5468f1adafaccf2cac5231e47
Jenkins Infra Bot (updatecli) <60776566+jenkins-infra-bot@users.noreply.github.com>
2022-09-05 09:37:26 +0000

    chore: Updated the content of the file "/tmp/updatecli/jenkins-infra/aw...
    ... s/vpc.tf"
    
    Made with ❤️️ by updatecli

--- vpc.tf
+++ vpc.tf
@@ -7,7 +7,7 @@ resource "random_string" "suffix" {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "3.14.2"
+  version = "3.14.3"
 
   name = "${local.cluster_name}-vpc"
   cidr = "10.0.0.0/16"

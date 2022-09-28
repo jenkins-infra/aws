a7cb238c87f663ceb7c51687a5b5580a5c60937e
Hervé Le Meur <hlemeur@cloudbees.com>
2022-09-28 18:56:23 -0400

    fix(eks-public): add provider kubernetes to be able to update aws-auth configmap

--- eks-public-cluster.tf
+++ eks-public-cluster.tf
@@ -117,3 +117,9 @@ data "aws_eks_cluster" "public-cluster" {
 data "aws_eks_cluster_auth" "public-cluster" {
   name = module.eks-public.cluster_id
 }
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.public-cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.public-cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.public-cluster.token
+}

4da3f42fdd1fdc00e3402457a4910fd49f771ed9
Hervé Le Meur <hlemeur@cloudbees.com>
2022-09-29 11:39:17 -0400

    fix: set specific kubernetes provider in eks-public module
    So this specific kubeconfig would be used for this cluster instead of the module one

--- providers.tf
+++ providers.tf
@@ -6,6 +6,7 @@ provider "local" {
 }
 
 provider "kubernetes" {
+  alias                  = "eks-public"
   host                   = data.aws_eks_cluster.public-cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.public-cluster.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.public-cluster.token

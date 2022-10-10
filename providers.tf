6f637b3028ddb28bd8dcf13f2ca818bf4e8326b5
Hervé Le Meur <91831478+lemeurherve@users.noreply.github.com>
2022-10-10 16:42:02 +0200

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -4,10 +4,3 @@ provider "aws" {
 
 provider "local" {
 }
-
-provider "kubernetes" {
-  alias                  = "eks-public"
-  host                   = data.aws_eks_cluster.public-cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.public-cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.public-cluster.token
-}

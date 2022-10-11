4e607eeb3c32a8f46a25d2d6159ca4738ab64d63
Hervé Le Meur <91831478+lemeurherve@users.noreply.github.com>
2022-10-11 12:04:46 +0200

    Revert "chore: temporary remove eks-public cluster" and set own private_subnets for eks-public (#226)
    * Revert "chore: temporary remove eks-public cluster (#224)"
    
    This reverts commit bb422e58f43a58515475e26ec26c42b21114891c.
    
    * fix: use own private_subnets for eks-public

---
+++ iam-role-nlb.tf
@@ -0,0 +1,17 @@
+module "eks_iam_role_nlb" {
+  source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version                       = "5.5.0"
+  create_role                   = true
+  role_name                     = local.nlb_account_name
+  provider_url                  = replace(module.eks-public.cluster_oidc_issuer_url, "https://", "")
+  role_policy_arns              = [aws_iam_policy.cluster_nlb.arn]
+  oidc_fully_qualified_subjects = ["system:serviceaccount:${local.nlb_account_namespace}:${local.nlb_account_name}"]
+}
+
+resource "aws_iam_policy" "cluster_nlb" {
+  name        = "AWSLoadBalancerControllerIAMPolicy"
+  description = "EKS cluster-nlb policy"
+  # JSON from https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.4.3/docs/install/iam_policy.json
+  # Cf https://docs.aws.amazon.com/eks/latest/userguide/aws-load-balancer-controller.html
+  policy = file("iam-nlb-policy.json") #tfsec:ignore:aws-iam-no-policy-wildcards
+}

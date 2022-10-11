5eda24a9b8bc4f1ca9547a85eefe20a218eaf45a
Hervé Le Meur <hlemeur@cloudbees.com>
2022-10-11 14:15:58 +0200

    fixup: unique autoscaler role names
    Fixup of https://github.com/jenkins-infra/aws/pull/232

--- locals.tf
+++ locals.tf
@@ -15,12 +15,12 @@ locals {
   ec2_agents_publickeys = compact(split("\n", file("./ec2_agents_authorized_keys")))
 
   # EKS related
-  cluster_name                             = "jenkins-infra-eks-${random_string.suffix.result}"
-  public_cluster_name                      = "public-${random_pet.suffix_public.id}"
-  k8s_autoscaler_service_account_namespace = "autoscaler"
-  k8s_autoscaler_service_account_name      = "cluster-autoscaler-aws-cluster-autoscaler-chart"
-  nlb_account_namespace                    = "nlb"
-  nlb_account_name                         = "aws-load-balancer-controller"
+  cluster_name                 = "jenkins-infra-eks-${random_string.suffix.result}"
+  public_cluster_name          = "public-${random_pet.suffix_public.id}"
+  autoscaler_account_namespace = "autoscaler"
+  autoscaler_account_name      = "cluster-autoscaler-aws-cluster-autoscaler-chart"
+  nlb_account_namespace        = "nlb"
+  nlb_account_name             = "aws-load-balancer-controller"
   #AWS security groups related
   aws_security_groups = ["infraci:infra.ci.jenkins.io:20.72.105.159/32", "release:release.ci.jenkins.io:52.177.88.13/32"]
 

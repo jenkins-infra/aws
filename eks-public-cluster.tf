58ff64bf9568eb0d1ab2c08047e476e4f0e26eed
Damien Duportal <damien.duportal@gmail.com>
2022-12-19 13:41:53 +0100

    eks: remove resolve_conflicts directive (as default) for cluster_addons - https://github.com/terraform-aws-modules/terraform-aws-eks/blob/master/docs/UPGRADE-19.0.md
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- eks-public-cluster.tf
+++ eks-public-cluster.tf
@@ -47,19 +47,15 @@ module "eks-public" {
   cluster_addons = {
     coredns = {
       addon_version     = "v1.8.7-eksbuild.3"
-      resolve_conflicts = "OVERWRITE"
     }
     kube-proxy = {
       addon_version     = "v1.23.8-eksbuild.2"
-      resolve_conflicts = "OVERWRITE"
     }
     vpc-cni = {
       addon_version     = "v1.11.4-eksbuild.1"
-      resolve_conflicts = "OVERWRITE"
     }
     aws-ebs-csi-driver = {
       addon_version            = "v1.11.4-eksbuild.1"
-      resolve_conflicts        = "OVERWRITE"
       service_account_role_arn = module.eks-public_irsa_ebs.iam_role_arn
     }
   }

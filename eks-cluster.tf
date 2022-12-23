6c018b968a6fa6c60d9b35000e665c263ea3edfd
Jenkins Infra Bot (updatecli) <60776566+jenkins-infra-bot@users.noreply.github.com>
2022-12-23 11:55:38 +0000

    chore: Updated the content of the file "/tmp/updatecli/github/jenkins...
    ... -infra/aws/eks-cluster.tf"
    Updated the content of the file "/tmp/updatecli/github/jenkins-infra/aws/eks-public-cluster.tf"
    
    Made with ❤️️ by updatecli

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -11,7 +11,7 @@ resource "aws_kms_key" "eks" {
 # EKS Cluster definition
 module "eks" {
   source       = "terraform-aws-modules/eks/aws"
-  version      = "19.4.0"
+  version      = "19.4.2"
   cluster_name = local.cluster_name
   # Kubernetes version in format '<MINOR>.<MINOR>', as per https://docs.aws.amazon.com/eks/latest/userguide/kubernetes-versions.html
   cluster_version = "1.23"

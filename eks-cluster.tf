7c80bd780a86260e72c8b047a1215a7016b4daf1
Jenkins Infra Bot (updatecli) <60776566+jenkins-infra-bot@users.noreply.github.com>
2023-02-16 11:19:45 +0000

    chore: Updated the content of the file "/tmp/updatecli/github/jenkins...
    ... -infra/aws/eks-public-cluster.tf"
    Updated the content of the file "/tmp/updatecli/github/jenkins-infra/aws/eks-cluster.tf"
    
    Made with ❤️️ by updatecli

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -124,7 +124,7 @@ module "eks" {
 
 module "eks_iam_role_autoscaler" {
   source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version                       = "5.11.1"
+  version                       = "5.11.2"
   create_role                   = true
   role_name                     = "${local.autoscaler_account_name}-eks"
   provider_url                  = replace(module.eks.cluster_oidc_issuer_url, "https://", "")
@@ -138,7 +138,7 @@ module "eks_iam_role_autoscaler" {
 
 module "eks_irsa_ebs" {
   source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version                       = "5.11.1"
+  version                       = "5.11.2"
   create_role                   = true
   role_name                     = "${local.ebs_account_name}-eks"
   provider_url                  = replace(module.eks.cluster_oidc_issuer_url, "https://", "")

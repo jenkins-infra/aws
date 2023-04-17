586fde061356513609fe4f2014a11ddcec849c35
Damien Duportal <damien.duportal@gmail.com>
2023-04-17 09:47:51 +0200

    feat(eks) split and optimize node pools for ci.jenkins.io usage (bom vs. plugins) (#388)
    * fix(eks) select cheaper instances with a lower eviction rate
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>
    
    * feat(eks) add a new node pool for BOM builds with 23 pods per node
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>
    
    ---------
    
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -87,16 +87,21 @@ module "eks" {
     },
     # This list of worker pool is aimed at mixed spot instances type, to ensure that we always get the most available (e.g. the cheaper) spot size
     # as per https://aws.amazon.com/blogs/compute/cost-optimization-and-resilience-eks-with-spot-instances/
+    # Pricing table for 2023: https://docs.google.com/spreadsheets/d/1_C0I0jE-X0e0vDcdKOFIWcnwpOqWC8RQ4YOCgXNnplY/edit?usp=sharing
     spot_linux_4xlarge = {
+      # 4xlarge: Instances supporting 3 pods (limited to 4 vCPUs/8 Gb) each with 1 vCPU/1Gb margin
       name          = "spot-linux-4xlarge"
       capacity_type = "SPOT"
-      # Instances of 16 vCPUs /	64 Gb each
-      instance_types = ["m5.4xlarge", "m5d.4xlarge", "m5a.4xlarge", "m5ad.4xlarge", "m5n.4xlarge", "m5dn.4xlarge"]
+      # Less than 5% eviction rate, cost below $0.08 per pod per hour
+      instance_types = [
+        "c5.4xlarge",
+        "c5a.4xlarge"
+      ]
       block_device_mappings = {
         xvda = {
           device_name = "/dev/xvda"
           ebs = {
-            volume_size           = 90 # With 3 pods / machine, that can use ~25 Gb each at the same time (`emptyDir`)
+            volume_size           = 90 # With 3 pods / machine, that can use ~30 Gb each at the same time (`emptyDir`)
             volume_type           = "gp3"
             iops                  = 3000 # Max included with gp3 without additional cost
             throughput            = 125  # Max included with gp3 without additional cost
@@ -105,16 +110,61 @@ module "eks" {
           }
         }
       }
-      spot_instance_pools = 6 # Amount of different instance that we can use
+      spot_instance_pools = 3 # Amount of different instance that we can use
       min_size            = 0
       max_size            = 50
-      desired_size        = 1
+      desired_size        = 0
+      kubelet_extra_args  = "--node-labels=node.kubernetes.io/lifecycle=spot"
+      tags = {
+        "k8s.io/cluster-autoscaler/enabled"               = true,
+        "k8s.io/cluster-autoscaler/${local.cluster_name}" = "owned",
+        "ci.jenkins.io/agents-density"                    = 3,
+      }
+      attach_cluster_primary_security_group = true
+    },
+    spot_linux_24xlarge = {
+      # 24xlarge: Instances supporting 23 pods (limited to 4 vCPUs/8 Gb) each with 1 vCPU/1Gb margin
+      name          = "spot-linux-24xlarge"
+      capacity_type = "SPOT"
+      # Less than 5% eviction rate, cost below $0.05 per pod per hour
+      instance_types = [
+        "m5.24xlarge",
+        "c5.24xlarge",
+      ]
+      block_device_mappings = {
+        xvda = {
+          device_name = "/dev/xvda"
+          ebs = {
+            volume_size           = 575 # With 23 pods / machine, that can use ~25 Gb each at the same time (`emptyDir`)
+            volume_type           = "gp3"
+            iops                  = 3000 # Max included with gp3 without additional cost
+            throughput            = 125  # Max included with gp3 without additional cost
+            encrypted             = false
+            delete_on_termination = true
+          }
+        }
+      }
+      spot_instance_pools = 2 # Amount of different instance that we can use
+      min_size            = 0
+      max_size            = 15
+      desired_size        = 0
       kubelet_extra_args  = "--node-labels=node.kubernetes.io/lifecycle=spot"
       tags = {
         "k8s.io/cluster-autoscaler/enabled"               = true,
         "k8s.io/cluster-autoscaler/${local.cluster_name}" = "owned",
       }
       attach_cluster_primary_security_group = true
+      labels         = {
+        "ci.jenkins.io/agents-density"                    = 23,
+      }
+      taints         = [
+        {
+          key    = "ci.jenkins.io/bom"
+          value  = "true"
+          effect = "NO_SCHEDULE"
+        }
+      ]
+      // TODO: taints + label (nodeselector): https://github.com/jenkins-infra/release/blob/41877fe2881e5b211550536ecb3d5b0123a08534/PodTemplates.d/package-windows.yaml#L39-L50
     },
   }
 

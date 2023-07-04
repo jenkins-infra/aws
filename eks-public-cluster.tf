d4ade3d912628fb2f44a1bdf5dedf717c325e758
Jenkins Infra Bot (updatecli) <60776566+jenkins-infra-bot@users.noreply.github.com>
2023-07-04 13:27:26 +0000

    chore: Updated the content of the file "/tmp/updatecli/github/jenkins...
    ... -infra/aws/cik8s-cluster.tf"
    Updated the content of the file "/tmp/updatecli/github/jenkins-infra/aws/eks-public-cluster.tf"
    
    Made with ❤️️ by updatecli

--- eks-public-cluster.tf
+++ eks-public-cluster.tf
@@ -179,7 +179,7 @@ resource "aws_iam_policy" "cluster_autoscaler_public" {
 
 module "eks_iam_assumable_role_autoscaler_eks_public" {
   source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version                       = "5.19.0"
+  version                       = "5.26.0"
   create_role                   = true
   role_name                     = "${local.autoscaler_account_name}-eks-public"
   provider_url                  = replace(module.eks-public.cluster_oidc_issuer_url, "https://", "")
@@ -193,7 +193,7 @@ module "eks_iam_assumable_role_autoscaler_eks_public" {
 
 module "eks-public_irsa_nlb" {
   source                        = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version                       = "5.19.0"
+  version                       = "5.26.0"
   create_role                   = true
   role_name                     = "${local.nlb_account_name}-eks-public"
   provider_url                  = replace(module.eks-public.cluster_oidc_issuer_url, "https://", "")
@@ -207,7 +207,7 @@ module "eks-public_irsa_nlb" {
 
 module "eks-public_irsa_ebs" {
   source                         = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version                        = "5.19.0"
+  version                        = "5.26.0"
   create_role                    = true
   role_name                      = "${local.ebs_account_name}-eks-public"
   provider_url                   = replace(module.eks-public.cluster_oidc_issuer_url, "https://", "")

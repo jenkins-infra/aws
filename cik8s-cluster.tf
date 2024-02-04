efb37ba15739e7820426bbeb9525b1ad66259a8e
Jenkins Infra Bot (updatecli) <60776566+jenkins-infra-bot@users.noreply.github.com>
2024-02-04 17:11:17 +0000

    chore: Update the Terraform module version of terraform-aws-modules/e...
    ... ks/aws in cik8s-cluster.tf
    
    Made with ❤️️ by updatecli

--- cik8s-cluster.tf
+++ cik8s-cluster.tf
@@ -11,7 +11,7 @@ resource "aws_kms_key" "cik8s" {
 # EKS Cluster definition
 module "cik8s" {
   source       = "terraform-aws-modules/eks/aws"
-  version      = "19.21.0"
+  version      = "20.0.1"
   cluster_name = local.cik8s_cluster_name
   # Kubernetes version in format '<MINOR>.<MINOR>', as per https://docs.aws.amazon.com/eks/latest/userguide/kubernetes-versions.html
   cluster_version = "1.26"

fa27d81ad3b109d1fa9fb30e4988cff7f8f6bb49
Damien Duportal <damien.duportal@gmail.com>
2024-07-25 19:51:05 +0200

    fix(census,pkg,usage) add HTTP NSG
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- pkg.origin.jenkins.io.tf
+++ pkg.origin.jenkins.io.tf
@@ -9,3 +9,8 @@ resource "aws_network_interface_sg_attachment" "ssh_to_pkg" {
   security_group_id    = aws_security_group.restricted_ssh.id
   network_interface_id = data.aws_instance.pkg_origin_jenkins_io.network_interface_id
 }
+
+resource "aws_network_interface_sg_attachment" "http_to_pkg" {
+  security_group_id    = aws_security_group.unrestricted_http.id
+  network_interface_id = data.aws_instance.pkg_origin_jenkins_io.network_interface_id
+}

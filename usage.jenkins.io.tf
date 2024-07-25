35fbd10bc2c3fb825d71c46ded7b9f1e75829dc0
Damien Duportal <damien.duportal@gmail.com>
2024-07-25 19:55:28 +0200

    fix(census,pkg,usage) add HTTP NSG (#559)
    Signed-off-by: Damien Duportal <damien.duportal@gmail.com>

--- usage.jenkins.io.tf
+++ usage.jenkins.io.tf
@@ -10,3 +10,8 @@ resource "aws_network_interface_sg_attachment" "ssh_to_usage" {
   security_group_id    = aws_security_group.restricted_ssh.id
   network_interface_id = data.aws_instance.usage_jenkins_io.network_interface_id
 }
+
+resource "aws_network_interface_sg_attachment" "http_to_usage" {
+  security_group_id    = aws_security_group.unrestricted_http.id
+  network_interface_id = data.aws_instance.usage_jenkins_io.network_interface_id
+}

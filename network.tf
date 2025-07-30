c2782bfb2953d7899112dce8fb272d6fb6284520
Damien Duportal <damien.duportal@gmail.com>
2025-07-30 17:46:10 +0200

    Update network.tf

--- network.tf
+++ network.tf
@@ -23,11 +23,9 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh_from_admins" {
     for ip in flatten(concat(
       module.jenkins_infra_shared_data.outbound_ips["trusted.ci.jenkins.io"],             # permanent agent of update_center2
       module.jenkins_infra_shared_data.outbound_ips["trusted.sponsorship.ci.jenkins.io"], # ephemeral agents for crawler
+      module.jenkins_infra_shared_data.outbound_ips["private.vpn.jenkins.io"],            # connections routed through the VPN
       # TODO: track with updatecli
-      ["20.122.14.108", "20.186.70.154"],                                      # Outbound IPv4 of the infracijioagents-1-sponsorship NAT gateway (infra.ci agents)
-      module.jenkins_infra_shared_data.outbound_ips["private.vpn.jenkins.io"], # connections routed through the VPN
-      # TODO: track with updatecli
-      ["172.200.139.164", "128.24.89.148"], # Outbound IPv4 of the privatek8s-sponsorship.jenkins.io NAT gateway (release.ci agents, controller and infra.ci controller)
+      ["172.210.200.59", "20.10.193.4"],                                                  # Outbound IPv4 of the infra.ci.jenkins.io agents NAT gateway
     )) : ip
     if can(cidrnetmask("${ip}/32"))
   ])
